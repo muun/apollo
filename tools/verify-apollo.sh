fa1153cf50eaa5c9d16c53020ce6ead307076907
acrespo <alvaro.andres.crespo@gmail.com>
2025-05-30 19:02:54 -0300

    Apollo: Release source code for 53.4

--- tools/verify-apollo.sh
+++ tools/verify-apollo.sh
@@ -19,22 +19,76 @@ cd $(git rev-parse --show-toplevel)
 
 tmp=$(mktemp -d)
 
+# Ensure temp directory is deleted when script exits
+trap 'rm -rf "$tmp"' EXIT
+
 # Prepare paths to extract APKs
 mkdir -p "$tmp/to_verify" "$tmp/baseline"
 
-echo "Building the APK from source. This might take a while (10-20 minutes)..."
-
+echo "Building the APKs from source. This might take a while (10-20 minutes)..."
 mkdir -p apk
 DOCKER_BUILDKIT=1 docker build -f android/Dockerfile -o apk .
 
-unzip -q -d "$tmp/to_verify" "$apk_to_verify"
-unzip -q -d "$tmp/baseline" "apk/apolloui-prod-release-unsigned.apk"
+# Clean to_verify directory before unzipping
+rm -rf "$tmp/to_verify"
+
+# Unzip the APK to verify
+mkdir -p "$tmp/to_verify"
+unzip -q -o "$apk_to_verify" -d "$tmp/to_verify"
 
 # TODO: verify the signature
 
 # Remove the signature since OSS users won't have Muuns private signing key
-rm -r "$tmp"/{to_verify,baseline}/{META-INF,resources.arsc}
+rm -r "$tmp"/to_verify/{META-INF,resources.arsc}
 
-diff -r "$tmp/to_verify" "$tmp/baseline" && echo "Verification success!" || echo "Verification failed :("
+# Compare /lib first
+lib_dirs_in_verify=($(find "$tmp/to_verify/lib" -mindepth 1 -maxdepth 1 -type d 2>/dev/null || true))
 
+echo "Found lib directory in APK to verify: ${lib_dirs_in_verify[*]}"
 
+if [ ${#lib_dirs_in_verify[@]} -ne 1 ]; then
+    echo "Unexpected lib directory structure in APK to verify."
+    exit 3
+fi
+
+lib_dir_name=$(basename "${lib_dirs_in_verify[0]}")
+echo "Using lib architecture: $lib_dir_name"
+
+baseline_apk_dir=""
+
+# Pick baseline based on lib architecture
+case "$lib_dir_name" in
+  arm64-v8a)
+    baseline_apk_dir="apk/apolloui-prod-arm64-v8a-release-unsigned.apk"
+    ;;
+  armeabi-v7a)
+    baseline_apk_dir="apk/apolloui-prod-armeabi-v7a-release-unsigned.apk"
+    ;;
+  x86)
+    baseline_apk_dir="apk/apolloui-prod-x86-release-unsigned.apk"
+    ;;
+  x86_64)
+    baseline_apk_dir="apk/apolloui-prod-x86_64-release-unsigned.apk"
+    ;;
+  *)
+    echo "Unknown architecture: $lib_dir_name"
+    exit 4
+    ;;
+esac
+
+# Clean baseline directory before unzipping
+rm -rf "$tmp/baseline"/*
+
+unzip -q -o "$baseline_apk_dir" -d "$tmp/baseline"
+rm -r "$tmp"/baseline/{META-INF,resources.arsc}
+
+echo "Comparing files..."
+
+diff_non_lib=$(diff -r "$tmp/to_verify" "$tmp/baseline" || true)
+
+if [ -n "$diff_non_lib" ]; then
+    echo "Verification failed :("
+    exit 5
+fi
+
+echo "Verification success!"
